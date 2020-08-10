811ade7f0251a7332bc89e0a956f557d45c5641a
Thamara Andrade <tkcandrade@gmail.com>
2020-08-10 06:09:26 -0300

    Improving markdown report

--- src/reporting.ts
+++ src/reporting.ts
@@ -2,15 +2,29 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 
-import { getNotes } from './note-db';
+import { getNotes, Note } from './note-db';
+import { getRelativePathForFileName } from './utils'
+
+const getCodeSnippetString = (note: Note): string => {
+    const moreThanOneLine = note.positionEnd.line != note.positionStart.line;
+    const firstLineOffset = moreThanOneLine ? note.positionStart.character : 0;
+    let codeSnippet = note.codeSnippet;
+    if (moreThanOneLine && firstLineOffset) {
+        const offsetSpace = Array(firstLineOffset + 1).join(' ');
+        codeSnippet = offsetSpace + codeSnippet;
+    }
+    return codeSnippet;
+}
 
 // TODO: We should use Jinja or something like this to generate these markdown files
-export const getNoteInMarkdown = (text: string, fileName: string, codeSnippet: string): string => {
-    let result = `### ${text}\n\n`;
-    result += `${fileName}\n`;
-    result += '```\n';
-    result += `${codeSnippet}\n`;
-    result += '```\n';
+export const getNoteInMarkdown = (note: Note): string => {
+    let result = `### - ${note.text}\n\n`;
+    if (note.fileName.length > 0) {
+        result += `\`${getRelativePathForFileName(note.fileName)}\`\n\n`;
+        result += '```\n';
+        result += `${getCodeSnippetString(note)}\n`;
+        result += '```\n';
+    }
     return result;
 };
 
@@ -24,7 +38,7 @@ export const getNotesInMarkdown = (): string => {
     for (let i in notes) {
         const note = notes[i];
         if (note.status === 'pending') {
-            result += getNoteInMarkdown(note.text, note.fileName, note.codeSnippet);
+            result += getNoteInMarkdown(note);
         }
     }
 
@@ -34,7 +48,7 @@ export const getNotesInMarkdown = (): string => {
     for (let i in notes) {
         const note = notes[i];
         if (note.status !== 'pending') {
-            result += getNoteInMarkdown(note.text, note.fileName, note.codeSnippet);
+            result += getNoteInMarkdown(note);
         }
     }
 
