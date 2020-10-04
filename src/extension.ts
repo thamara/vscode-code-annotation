3c20739d0521564a6ceeec9d89e6a4e8b71680ca
Jack <40802798+Jack073@users.noreply.github.com>
2020-10-04 21:51:33 +0100

    lint and remove unnecessary duplicates

--- src/extension.ts
+++ src/extension.ts
@@ -24,7 +24,7 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.commands.registerCommand('code-annotation.openNoteFromId', (id: string) => {
         treeActions.openNoteFromId(id);
     });
-    
+
     vscode.commands.registerCommand('code-annotation.summary', () => {
         generateMarkdownReport();
     });
