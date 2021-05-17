c426acb5d1053b7fc2334cb860ab5e49570c966f
drewjel <andrew.james.elsey@gmail.com>
2021-05-16 21:24:20 -0400

    Minor changes to instal script

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
-cd /peirce/Peirce-vscode-api
-pip install -r requirements.txt
-
 nvm install node
 nvm use node
 npm install
 npm install -g vsce
 vsce package
 code --install-extension /peirce/Peirce-vscode/code-annotation-0.0.1.vsix
+
+cd /peirce/Peirce-vscode-api
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+nohup python3 src/api.py &
