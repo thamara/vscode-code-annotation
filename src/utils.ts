fb59aa4a0f9a7461c7e66b3cd8f483d4aa80d2bc
Thamara Andrade <tkcandrade@gmail.com>
2021-07-08 01:33:20 -0300

    Fix getRelativePathForFileName to work on windows

--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,9 @@ import * as vscode from 'vscode';
 
 export const getRelativePathForFileName = (fullPathFileName: string): string => {
     const workspacePath = vscode.workspace.rootPath;
-    let relativePath = workspacePath;
     if (workspacePath) {
-        relativePath = fullPathFileName.replace(workspacePath, '');
-        if (relativePath && relativePath.charAt(0) === '/') {
+        let relativePath = fullPathFileName.replace(workspacePath, '');
+        if (relativePath && (relativePath.charAt(0) === '/' || relativePath.charAt(0) === '\\')) {
             relativePath = relativePath.substr(1);
             return relativePath;
         }
